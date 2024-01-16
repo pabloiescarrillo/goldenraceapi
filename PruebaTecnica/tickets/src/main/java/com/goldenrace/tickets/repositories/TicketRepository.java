1814e639ae4b9170021155f4b897cd42c5c3c383
pabloiescarrillo <146716452+pabloiescarrillo@users.noreply.github.com>
2024-01-16 01:17:07 +0100

    Add files via upload

---
+++ PruebaTecnica/tickets/src/main/java/com/goldenrace/tickets/repositories/TicketRepository.java
@@ -0,0 +1,16 @@
+package com.goldenrace.tickets.repositories;
+
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.goldenrace.tickets.models.Ticket;
+
+@Repository
+public interface TicketRepository extends CrudRepository<Ticket, Integer> {
+
+	List<Ticket> findByCreationDateBetween(LocalDateTime to, LocalDateTime from);
+}
